5d9b6de1a8b7380bc7ad1b6ea4d533040123b0bf
MacDudeDude <89434264+MacDudeDude@users.noreply.github.com>
2025-04-13 04:05:28 -0700

    Began enemy pack

---
+++ Code/EnemyPackInitializer.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@export var fleetManager: Node
+@export var enemiesMinMax: Vector2
+@export var enemyBodyParts: Array[Body]
+@export var enemyWheelParts: Array[Wheel]
+@export var enemyWeaponParts: Array[Weapon]
+
+
+func _ready():
+	pass
